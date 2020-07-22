d8d46d13cc72197208c833aa2902f5178e0776e5
Dongjoon Kim <kdj924@gmail.com>
2020-07-22 18:08:26 +0900

    added the Listpage

--- src/List.tsx
+++ src/List.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Game from './data/game.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GameImage from './images/Games.jpg';
+import './css/Home.css';
 
 function List() {
   return (
     <div>
-      <h1>List</h1>
+      <Container className="p-3">
+        <h1 className="text-left">List</h1>
+        <ul>
+          {Game.games.map(x => {
+            return (
+              <>
+                <table>
+                  <tr>
+                    <th>{x.title}</th>
+                    <th className="left">{x.description}</th>
+                    <th><img className="game_list" src={process.env.PUBLIC_URL + '/images/' + x.images} alt="game_images " /></th>
+                  </tr>
+                </table>
+                {/* <li>{x.title} | {x.description}
+                <img className="game_list" src={process.env.PUBLIC_URL + '/images/' + x.images} alt="game_images " />
+              </li> */}
+              </>
+            );
+          })}
+        </ul>
+      </Container>
     </div>
   );
 }
